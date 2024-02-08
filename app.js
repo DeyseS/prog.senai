d44122af3c226ea9e3e45830d16a6c209df8f79e
Deyse <122042379+DeyseS@users.noreply.github.com>
2024-02-08 15:16:40 -0300

    mod08.02

---
+++ app.js
@@ -0,0 +1,44 @@
+ let variavelA = 10   //Inteiro
+ let variavelB = 2    //Inteiro
+ let variavelC = "2"  //string   
+
+ document.write('Variavel A: ${variavelA}<br>')
+ document.write('Variavel B: ${variavelB}<br>')
+ document.write('Variavel C: ${variavelC}<br>')
+
+ console.log(variavelA = variavelB)
+ console.log(variavelA - variavelB)
+ console.log(variavelA * variavelB)
+ console.log(variavelA / variavelB)
+ console.log(variavelA % variavelB)
+
+ document.write('variavel A + variavel B =${variavelA + variavelB} <br>')
+
+ console.log('E Igual')
+ console.log(variavelB == variavelC) //Comparaçao
+
+ console.log('E Identico')
+ console.log(variavelB === variavelC) //identico
+
+ console.log('E Diferente')
+ console.log(variavelB != variavelC) //diferente
+
+ console.log('Nao e Identico')
+ console.log(variavelB !== variavelC) //nao e identico
+
+ console.log ('Menor, maior..')
+ console.log (variavelA < variavelB)
+ console.log (variavelA > variavelB)
+ console.log ( variavelA <= variavelB)
+ console.log (variavelA >= variavelB )
+
+ let variavelD = true
+ let variavelE = false
+ document.write('<p> Variavel D: ${variavelD} <br>')
+ document.write('<p> Variavel E: ${variavelE}<br>')
+
+ console.log('&& e ||')
+ console.log(variavelD && variavelE)
+ console.log(variavelD || variavelE)
+ console.log(!variavelD)
+ console.log(variavelD)
